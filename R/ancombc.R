8e8e5874ca88548834b76e8259ea26f6da6f129b
Huang Lin <huanglinfrederick@gmail.com>
2020-09-13 19:08:12 +0800

    Using the GlobalPatterns dataset in the examples.

--- R/ancombc.R
+++ R/ancombc.R
@@ -127,34 +127,21 @@
 #'
 #' #========================Run ANCOMBC Using a Real Data=======================
 #'
-#' library(microbiome)
+#' library(phyloseq)
 #' library(tidyverse)
-#' data(atlas1006)
-#' # Subset to baseline
-#' pseq = subset_samples(atlas1006, time == 0)
-#' # Re-code the bmi group
-#' sample_data(pseq)$bmi_group = recode(sample_data(pseq)$bmi_group,
-#'                                      `underweight` = "lean",
-#'                                      `lean` = "lean",
-#'                                      `overweight` = "overweight",
-#'                                      `obese` = "obese",
-#'                                      `severeobese` = "obese",
-#'                                      `morbidobese` = "obese")
-#' # Re-code the nationality group
-#' sample_data(pseq)$nation = recode(sample_data(pseq)$nationality,
-#'                                   `Scandinavia` = "NE",
-#'                                   `UKIE` = "NE",
-#'                                   `SouthEurope` = "SE",
-#'                                   `CentralEurope` = "CE",
-#'                                   `EasternEurope` = "EE")
+#' data(GlobalPatterns)
 #'
 #' # Aggregate to phylum level
-#' phylum_data = aggregate_taxa(pseq, "Phylum")
+#' phylum_data = tax_glom(GlobalPatterns, "Phylum")
+#' # The taxonomy table
+#' tax_mat = as(tax_table(phylum_data), "matrix")
 #'
-#' out = ancombc(phyloseq = phylum_data, formula = "age + nation + bmi_group",
+#' # Run ancombc function
+#' out = ancombc(phyloseq = phylum_data, formula = "SampleType",
 #'               p_adj_method = "holm", zero_cut = 0.90, lib_cut = 1000,
-#'               group = "nation", struc_zero = TRUE, neg_lb = TRUE, tol = 1e-5,
-#'               max_iter = 100, conserve = TRUE, alpha = 0.05, global = TRUE)
+#'               group = "SampleType", struc_zero = TRUE, neg_lb = FALSE,
+#'               tol = 1e-5, max_iter = 100, conserve = TRUE,
+#'               alpha = 0.05, global = TRUE)
 #'
 #' res = out$res
 #' res_global = out$res_global
