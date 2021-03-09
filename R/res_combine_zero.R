20d672480b4d98c1d5d986513de038a6ae95fb2b
Huang Lin <huanglinfrederick@gmail.com>
2021-03-09 20:11:29 +0800

    bugs fix: fix the bug of inflated p-values and inconsistent output formats.

--- R/res_combine_zero.R
+++ R/res_combine_zero.R
@@ -5,16 +5,17 @@ res_combine_zero = function(x, group, struc_zero, zero_ind, alpha,
     # Set p/q-values of structural zeros to be 0s.
     if (struc_zero) {
         group_ind = grepl(group, covariates)
-        zero_mask = zero_ind - zero_ind[, 1]
-        zero_mask = 1 - zero_mask[, -1, drop = FALSE]
-        colnames(zero_mask) = covariates[group_ind]
+        zero_mask = 1 - apply(zero_ind, 1, function(x)
+            sum(x) > 0 & sum(x) < ncol(zero_ind))
         res$p_val[, group_ind] = res$p_val[, group_ind] * zero_mask
         res$q_val[, group_ind] = res$q_val[, group_ind] * zero_mask
-        res$diff_abn = res$q_val < alpha & !is.na(res$q_val)
+        res$diff_abn = data.frame(res$q_val < alpha & !is.na(res$q_val),
+                                  check.names = FALSE)
 
         # Global test
         if (global) {
-            zero_mask = 1 - apply(zero_ind, 1, function(x) any(x == 1))
+            zero_mask = 1 - apply(zero_ind, 1, function(x)
+                sum(x) > 0 & sum(x) < ncol(zero_ind))
             res_global[, "p_val"] = res_global[, "p_val"] * zero_mask
             res_global[, "q_val"] = res_global[, "q_val"] * zero_mask
             res_global[, "diff_abn"] = res_global[, "q_val"] < alpha &
